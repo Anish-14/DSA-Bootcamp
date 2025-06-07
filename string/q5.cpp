9c91a7d625b57d333069dda911d080d2611a2f3d
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-07 14:15:56 +0530

    Create q5.cpp

---
+++ string/q5.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/isomorphic-strings/description/?envType=problem-list-v2&envId=string
+// isomorphic strings
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, int> charIndexS;
+        unordered_map<char, int> charIndexT;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charIndexS.find(s[i]) == charIndexS.end()) {
+                charIndexS[s[i]] = i;
+            }
+
+            if (charIndexT.find(t[i]) == charIndexT.end()) {
+                charIndexT[t[i]] = i;
+            }
+
+            if (charIndexS[s[i]] != charIndexT[t[i]]) {
+                return false;
+            }
+        }
+
+        return true;   
+    }
+};
