8dceec22dffcd9d93fecb778a96ac8c6307469af
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-07 14:29:38 +0530

    Update q6.cpp

--- string/q6.cpp
+++ string/q6.cpp
@@ -1,1 +1,27 @@
+// https://leetcode.com/problems/number-of-segments-in-a-string/?envType=problem-list-v2&envId=string
+// number of segments in string
 
+class Solution {
+public:
+    int countSegments(string s) {
+        unordered_map<string,int>mpp;
+        int count=0;
+        string ans = "";
+        for(int i=0;i<s.length();i++){
+            if(s[i]!=' '){
+                ans+=s[i];
+            }
+            else if(s[i]==' '){
+                if(ans.length() != 0){
+                    mpp[ans]++;
+                    ans="";  
+                }
+            }
+        }
+        if(ans.length()!=0) mpp[ans]++;
+        for(auto it:mpp){
+            count += it.second;
+        }
+        return count;
+    }
+};
