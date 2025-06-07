89076313971bea7b5ca42cb13e9f324cfe5fcf8d
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-07 14:18:49 +0530

    Update q5.cpp

--- string/q5.cpp
+++ string/q5.cpp
@@ -24,3 +24,25 @@ public:
         return true;   
     }
 };
+
+// the following solution uses the same concept just the space complexity is constant
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        //optimal solution, TS = O(N), SC = 0(1)
+        //declare the hash maps
+        int s_map[128] = {0};
+        int t_map[128] = {0};
+
+        for(int i = 0; i<s.size(); i++){
+            if(s_map[s[i]] != t_map[t[i]])
+            return false;
+
+            //assign values with i +=1;
+            s_map[s[i]] = i+1;
+            t_map[t[i]] = i+1;
+        }
+    return true;
+    }
+};
