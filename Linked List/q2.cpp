d79a47fded17f4fdad3e13c880b4f3db2ac09c8a
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-09 15:29:42 +0530

    Update q2.cpp

--- Linked List/q2.cpp
+++ Linked List/q2.cpp
@@ -1,1 +1,29 @@
+// https://leetcode.com/problems/remove-linked-list-elements/description/
+// Remove element from LL
 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL){
+            return head;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr){
+            if(curr->val == val){
+                if(prev == NULL || head->val == val){   // first Node , prev can be non NULL
+                    head = curr->next;                  // for head node also, because we are
+                }                                       // not deleting the Nodes
+                else{
+                    prev->next = curr->next;            // head ke alava delete karna
+                    curr = curr->next;
+                    continue;
+                }
+            }
+            prev = curr;            // normal aage badho
+            curr = curr->next;
+        }
+        return head;
+    }
+};
