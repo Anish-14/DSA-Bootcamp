50c8588956c1a2baaac7b43aba463d61deee5657
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-10 12:20:01 +0530

    Create q5.cpp

---
+++ Linked List/q5.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/palindrome-linked-list/description/
+// pallindrome LL
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        // reverse the second half of LL
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            if (first->val != second->val) {
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;  
+    }
+};
