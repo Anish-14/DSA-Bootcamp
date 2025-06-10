6d0a4001a9bd6d572aa087465f643c1d7b720f7d
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-10 12:36:40 +0530

    Create q6.cpp

---
+++ Linked List/q6.cpp
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+// Intersection of two LL
+
+
+// both the solution are for O(m+n)
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int n = 0;
+		int m = 0;
+		ListNode* ptr1 = headA;
+		ListNode* ptr2 = headB;
+		while(ptr1 != NULL){
+			n++;
+			ptr1 = ptr1 -> next;
+		}
+		while(ptr2 != NULL){
+			m++;
+			ptr2 = ptr2 -> next;
+		}
+		int t = abs(n - m);
+		if(n > m){
+			while(t){
+				headA = headA -> next;
+				t--;
+			}
+		}
+		else{
+			while(t){
+				headB = headB -> next;
+				t--;
+			}
+		}
+		while(headA != NULL and headB != NULL){
+			if(headA == headB){
+				return headA;
+			}
+			headA = headA -> next;
+			headB = headB -> next;
+		}
+		return NULL;
+    }
+};
+
+// another method
+// in this method at max from second round both nodes resonate with each other and hence comparison for the coommon node becomes possible
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *ptr1 = headA;
+		ListNode *ptr2 = headB;
+		while(ptr1 != ptr2){
+			if(ptr1 == NULL){
+				ptr1 = headB;
+			}
+			else{
+				ptr1 = ptr1 -> next;
+			}
+			if(ptr2 == NULL){
+				ptr2 = headA;
+			}
+			else{
+				ptr2 = ptr2 -> next;
+			}
+		}
+		return ptr1;
+    }
+};
