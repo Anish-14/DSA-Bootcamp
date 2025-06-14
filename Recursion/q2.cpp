4244100e5227513e82d891503a46286291c23ba7
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-14 22:58:41 +0530

    Create q2.cpp

---
+++ Recursion/q2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(vector<string> &ans, int n, string str, int open, int close){
+        if(open > n || close > n){
+            return ;
+        }
+        if(str.length() == 2*n){
+            ans.push_back(str);
+            return ;
+        }
+        
+        if(close < n)
+            solve(ans, n, str + '(', open + 1, close);
+        
+        if(open > close)
+            solve(ans, n, str + ')', open, close + 1);        
+
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        solve(ans,n,"",0,0);
+
+        return ans;
+    }
+};
