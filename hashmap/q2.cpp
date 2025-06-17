5d712548d3e338facbb5bb593abe8f14bb556d28
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-17 10:45:28 +0530

    Update q2.cpp

--- hashmap/q2.cpp
+++ hashmap/q2.cpp
@@ -22,3 +22,24 @@ public:
         return ans;
     }
 };
+
+
+// another method
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>map;
+        for(string s : strs)
+        {
+            string key = s;
+            sort(key.begin(),key.end());
+            map[key].push_back(s);
+        }
+        vector<vector<string>>result;
+        for(auto &pair : map)
+        {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
