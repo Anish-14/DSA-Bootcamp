043748c648a111f95a3652ef3c547f9ed47c390e
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-17 11:25:18 +0530

    Update q4.cpp

--- hashmap/q4.cpp
+++ hashmap/q4.cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/longest-consecutive-sequence/description/
 
-
+// O(n*logn) && O(n)
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
@@ -36,3 +36,54 @@ public:
         return maxi;
     }
 };
+
+
+// this is better solution than the above
+// O(n) && O(n)
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int> m;
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]=1;
+        }
+        for (auto &x: m)
+        {
+            int ele=x.first;
+            if(x.second==0)continue;
+            int sum=1;
+            int y=ele;
+            while(1)
+            {   
+                y++;
+                auto it=m.find(y);
+                if(it==m.end())break;
+                else
+                {
+                    sum++;
+                    m.erase(it);
+                }
+            }
+                y=ele;
+            while(1)
+            {   
+                y--;
+                auto it=m.find(y);
+                if(it==m.end())break;
+                else
+                {
+                    sum++;
+                    m.erase(it);
+                }
+            }
+            m[ele]=0;
+
+            ans=max(ans,sum);
+            
+        }
+        return ans;
+    }
+};
+
