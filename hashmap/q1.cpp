55ab7d724e96e8dd635db7e2305ac27ab58c5fbf
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-17 10:17:52 +0530

    Create q1.cpp

---
+++ hashmap/q1.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/ransom-note/description/
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> m;
+        for(int i = 0; i<magazine.length(); i++){
+            m[magazine[i]]++;
+        }
+
+        for(int i = 0; i<ransomNote.length(); i++){
+            if(m[ransomNote[i]] == 0){
+                return false;
+            }
+            m[ransomNote[i]]--;
+        }
+
+        return true;
+    }
+};
