a7b288b40bc908b225c029b51a7eb1632aeba1bb
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-29 12:01:06 +0530

    Create q6.cpp

---
+++ stack/q6.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/implement-stack-using-queues/description/
+
+
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i = 0; i<q.size() - 1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
