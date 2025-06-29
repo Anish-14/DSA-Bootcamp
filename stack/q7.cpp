96fde44a1c0509f848b4ba67d599904fd3c72525
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-29 12:33:23 +0530

    Update q7.cpp

--- stack/q7.cpp
+++ stack/q7.cpp
@@ -1,1 +1,25 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+// inorder traversal of binary tree without recursion (using stack)
 
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stack;
+
+        while (root != nullptr || !stack.empty()) {
+            while (root != nullptr) {
+                stack.push(root);
+                root = root->left;
+            }
+
+            root = stack.top();
+            stack.pop();
+            res.push_back(root->val);
+            root = root->right;
+        }
+
+        return res;        
+    }
+};
