61f87786269905249ce57842d3b3a1df9ec863db
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-30 12:03:38 +0530

    Update q3.cpp

--- queue/q3.cpp
+++ queue/q3.cpp
@@ -1,1 +1,24 @@
+// https://leetcode.com/problems/number-of-recent-calls/
 
+
+class RecentCounter {
+public:
+    queue<int>q; 
+    RecentCounter() {
+        int counter = 0 ; 
+    }
+    int ping(int t) {
+        if(q.empty()){
+            q.push(t) ;
+            return 1 ; 
+        }
+        // we don't need unnecessary comparison for q.front() > t as it will always be 
+        // because we are having input in increasing order
+        while(!q.empty()&&q.front()<t-3000){      
+            q.pop() ; 
+        }
+
+        q.push(t) ;
+        return q.size() ;  
+    }
+};
