08abda8e9e5d43a74f20bd76ede18042b7e56bd9
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-30 11:20:27 +0530

    Update q1.cpp

--- queue/q1.cpp
+++ queue/q1.cpp
@@ -1,1 +1,38 @@
+// https://leetcode.com/problems/implement-queue-using-stacks/description/
 
+class MyQueue {
+private:
+    stack<int> s;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(s.empty()){
+            s.push(x);
+            return;
+        }
+        int top = s.top();
+        s.pop();
+
+        push(x);
+
+        s.push(top);
+    }
+    
+    int pop() {
+        int top = s.top();
+        s.pop();
+
+        return top;
+    }
+    
+    int peek() {
+        return s.top();
+    }
+    
+    bool empty() {
+        return s.empty();
+    }
+};
