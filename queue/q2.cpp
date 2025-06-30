9a8f9c12fff2b0754d7add5cf52bec760220c9eb
Anish <154687537+Anish-14@users.noreply.github.com>
2025-06-30 11:48:25 +0530

    Update q2.cpp

--- queue/q2.cpp
+++ queue/q2.cpp
@@ -27,3 +27,25 @@ public:
         return vis[idx];
     }
 };
+
+
+
+// here is the queue solution for it
+
+int firstUniqChar(string s) {
+    unordered_map<char, int> count;
+    queue<pair<char, int>> q;
+
+    for (int i = 0; i < s.size(); ++i) {
+        count[s[i]]++;
+        q.push({s[i], i});
+
+        // Remove characters from the front that are no longer unique
+        while (!q.empty() && count[q.front().first] > 1) {
+            q.pop();
+        }
+    }
+
+    return q.empty() ? -1 : q.front().second;
+}
+
